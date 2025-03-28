ee9f9630a892d5b7904386128fac6132079c5c99
bayramalibirer <baryamali18@gmail.com>
2025-03-28 14:11:41 +0300

    main outline

--- app.vue
+++ app.vue
@@ -1,6 +1,3 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div class="text-center text-blue-500">Tailwind CSS Çalışıyor!</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss:{
+    cssPath: '~/assets/css/tailwind.css',
+  },
   devtools: { enabled: true }
 })
